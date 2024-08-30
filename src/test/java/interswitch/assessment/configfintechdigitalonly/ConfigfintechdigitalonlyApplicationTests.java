f505ad8c51ae1db8144e26a011e1c1bac60addad
creditvilleNg <chioma.chukelu@creditville.ng>
2024-08-30 18:16:15 +0100

    update

---
+++ src/test/java/interswitch/assessment/configfintechdigitalonly/ConfigfintechdigitalonlyApplicationTests.java
@@ -0,0 +1,13 @@
+package interswitch.assessment.configfintechdigitalonly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigfintechdigitalonlyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
